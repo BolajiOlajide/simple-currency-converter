629a3b95ba15a125353ad34766b26ccffbe7efbc
Bolaji Olajide <bolaji.olajide@andela.com>
2018-07-01 02:09:21 +0100

    feat(footer): complete footer implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
+import Footer from './components/Footer/Footer.component';
 import ConverterScreen from './pages/ConverterScreen/ConverterScreen.component';
 // import LoadingScreen from './pages/LoadingScreen/LoadingScreen.page';
 
 
 const App = () => <React.Fragment>
   <ConverterScreen />
+  <Footer />
 </React.Fragment>;
 
 export default App;
--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 
+// styles
+import './Footer.css';
 
-const Footer = () => <span>
-  Designed by
-  <a href="https://www.behance.net/scozky" target="_blank">
-    Bashir Aminu
-  </a>.Created with ❣️ by
-  <a href="https://github.com/BolajiOlajide" target="_blank">
+
+const Footer = () => <p className="footer">
+  Designed by <a href="https://www.behance.net/scozky" target="_blank">Bashir Aminu
+  </a>. Created with ❣️ by <a href="https://github.com/BolajiOlajide" target="_blank">
     BolajiOlajide
   </a>
-</span>;
+</p>;
 
 export default Footer;
